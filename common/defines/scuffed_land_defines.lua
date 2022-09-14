2e469b0ffa154dd651741458a13e63a33c08d9a9
atomicplan666 <50342682+atomicplan666@users.noreply.github.com>
2022-09-13 23:14:28 -0400

    AI and Date fixes
    - AI works, commented out define that broke
    - fixed the weird ass dates 1965.1.1.1 to 1965.1.1

--- common/defines/scuffed_land_defines.lua
+++ common/defines/scuffed_land_defines.lua
@@ -18,13 +18,14 @@ NDefines.NMilitary.UNIT_LEADER_MODIFIER_COOLDOWN_ON_GROUP_CHANGE = 3 -- WAS 15 |
 NDefines.NMilitary.TRAINING_ATTRITION = 0.00
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0
 NDefines.NMilitary.TRAINING_EXPERIENCE_SCALE = 62.0 -- 62.0
-NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5 -- WAS 1 aka TRAINED | Since the above was changed there is no point to not allowing divs to be trained to regular considering that its only 10% stats now. 
+
+-- NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL =  -- this breaks AI  
 NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 15000000    -- Increased so most nations don't need to queue up multiple lines of infantry or spam 2w infantry and convert 
 
 NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 0
 NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0
 
--- free templates
+-- -- free templates
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0 -- 0xp, was 25
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0 -- 0xp, was 5
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0 -- 0xp, was 10
@@ -36,10 +37,10 @@ NDefines.NProduction.RAILWAY_GUN_MAX_MIL_FACTORIES_PER_LINE = 300
 NDefines.NProduction.ANNEX_FIELD_EQUIPMENT_RATIO = 1	-- WAS 0.25 | Annex decisions should give all troops, but incase I screwed up here is this -Thrasymachus | How much equipment from deployed divisions will be transferred on annexation
 
 -- Free Designs(uselesss for now)
-NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 1				    -- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
-NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 1				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
-NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 1				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
-NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 1
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 3				    -- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 3				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 3				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 3
 
 --More XP
 NDefines.NMilitary.TRAINING_MAX_DAILY_COUNTRY_EXP = 0.16			-- (Def: 0.08)*2 | Maximum army XP gained per day from training
