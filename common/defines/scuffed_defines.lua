c52485cac348f97fd93fb2721adb1055e7c925bc
atomicplan666 <50342682+atomicplan666@users.noreply.github.com>
2023-04-23 17:57:54 -0400

    Define Fix

--- common/defines/scuffed_defines.lua
+++ common/defines/scuffed_defines.lua
@@ -38,7 +38,9 @@ NDefines.NMilitary.MAX_AIR_EXPERIENCE = 2000
 NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
-NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02, 0.04, 0.06, 0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
+NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02, 0.04, 0.06,	0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
+NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0.0015
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.03
 NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.9
 NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 1.2
 ----------------------------------
@@ -51,3 +53,5 @@ NDefines.NNavy.PRIDE_OF_THE_FLEET_UNASSIGN_COST = 0							-- cost to unassign/re
 NDefines.NNavy.PRIDE_OF_THE_FLEET_LOST_TEMP_MODIFIER_DURATION = 180	
 ----------------------------------
 NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30
+----------------------------------
+NDefines.NBuildings.MAX_SHARED_SLOTS = 35
