f93b649610c2f16f54918230c4729895248130ca
Endsiegg <119881933+Endsiegg@users.noreply.github.com>
2024-02-07 23:40:20 -0500

    TCDTF Defines Update

--- common/defines/scuffed_speed_defines.lua
+++ common/defines/scuffed_speed_defines.lua
@@ -1,4 +1,4 @@
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 750 -- Lag days
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 1000 -- pause days
-NDefines.NGame.GAME_SPEED_SECONDS = { 100.0, 0.25, 0.1, 0.05, 0.0 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 100.0, 0.2, 0.1, 0.05, 0.0 }
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
